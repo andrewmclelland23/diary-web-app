21001872a26b2d772fee84b41559c4b2eee92c62
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-29 12:21:29 +0000

    Refactored to display titles in an html list on /diary/list page

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'pg'
+require_relative 'lib/diary_entry'
 
 # Web app class
 class DiaryApp < Sinatra::Base
@@ -9,11 +10,12 @@ class DiaryApp < Sinatra::Base
   end
 
   get '/diary/list' do
-    DiaryEntry.list
+    @list = DiaryEntry.list
+    erb :'diary/list'
   end
 
   get '/diary/add' do
-    erb :add_entry
+    erb :'diary/add'
   end
 
   post '/diary/add' do
